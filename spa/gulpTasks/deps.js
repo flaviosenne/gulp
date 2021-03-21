dca213a21064303c3c059ea717dffbbc05df4a90
Joao flavio <flaviosenne123@gmail.com>
2021-03-21 16:58:52 -0300

    finish gulp to SPA

--- spa/gulpTasks/deps.js
+++ spa/gulpTasks/deps.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp')
+const uglify = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(callback){
-    return callback()
+function depsCSS(){
+
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+    .pipe(uglify({"uglyComments": false}))
+    .pipe(concat('deps.min.css'))
+    .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFonts(callback){
-    return callback()
+function depsFonts(){
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+    .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = { depsCSS, depsFonts}
--- spa/gulpTasks/server.js
+++ spa/gulpTasks/server.js
@@ -1,11 +1,25 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+
+function server(){
+    return gulp.src('build')
+    .pipe(webserver({
+        port: 8080,
+        open: true,
+        livereload: true
+    }))
+}
+
 
 function watchFile(callback){
+    watch('src/**/*.html', ()=> gulp.series('appHtml')())
+    watch('src/**/*.scss', ()=> gulp.series('appCSS')())
+    watch('src/**/*.js', ()=> gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', ()=> gulp.series('appImg')())
+
     return callback()
 }
 
-function server(callback){
-    return callback()
-}
 
 module.exports = { watchFile, server}
